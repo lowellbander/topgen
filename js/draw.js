ef27926ac7246f0942cac1d35e19ca9a7f384fd2
Lowell Bander <lowellbander@gmail.com>
2016-05-17 20:10:36 -0700

    i can draw simple nodes

--- js/draw.js
+++ js/draw.js
@@ -1,6 +1,20 @@
+var state = {};
+
+var setState = function (newState) {
+    state = newState;
+}
+
+var node = {};
+
+var SVG_NS = 'http://www.w3.org/2000/svg';
+
 function draw(scene) {
    scene.onclick = function (e) {
-       console.log("click");
+       var element = document.createElementNS(SVG_NS, 'circle');
+       element.setAttribute('cx', (e.clientX - 20).toString());
+       element.setAttribute('cy', (e.clientY - 20).toString());
+       element.setAttribute('r', '5');
+       scene.appendChild(element);
    }
 }
 
